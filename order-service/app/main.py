7dbd5a18fffb52db5b40e3e7dc83f7ec355f43e2
Zagir <azazoha@gmail.com>
2026-04-26 22:55:17 +0400

    add orders.py

--- order-service/app/main.py
+++ order-service/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from api.orders import orders_router
 
 app = FastAPI()
 
 @app.get("/health")
 async def health():
     return { "status": "ok" }
+
+
+app.include_router(orders_router)
